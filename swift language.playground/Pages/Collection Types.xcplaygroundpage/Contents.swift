8454063cefffdd72630c8415d8768f5e3ca237ef
rockgarden <rockgarden@sina.com>
2016-06-12 22:37:39 +0800

    opt

--- swift language.playground/Pages/Collection Types.xcplaygroundpage/Contents.swift
+++ swift language.playground/Pages/Collection Types.xcplaygroundpage/Contents.swift
@@ -2,7 +2,7 @@
 
 /*: 
  # Collection Types
- 数组(Arrays)类型的赋值和拷贝行为要比字典(Dictionary)类型的复杂的多.
+ 数组类型的赋值和拷贝行为要比字典(Dictionary)类型的复杂的多.
  当操作数组内容时,数组(Array)能提供接近C语言的的性能,并且拷贝行为只有在必要时才会发生.
  如果你将一个数组(Array)实例赋给一个变量或常量,或者将其作为参数传递给函数或方法调用,在事件发生时数组的内容不会被拷贝.
  相反,数组公用相同的元素序列.当你在一个数组内修改某一元素,修改结果也会在另一数组显示.
@@ -11,7 +11,6 @@
  - 插入(inserting)
  - 删除(removing)
  - 使用范围下标(ranged subscript)去替换这一范围内的元素.
- 
  只有当数组拷贝确要发生时,数组内容的行为规则与字典中键值的相同.
  - Note:
  确保数组的唯一性
@@ -19,8 +18,8 @@
  通过在数组变量上调用unshare方法来确定数组引用的唯一性。(当数组赋给常量时，不能调用unshare方法)
  如果一个数组被多个变量引用，在其中的一个变量上调用unshare方法，则会拷贝此数组，此时这个变量将会有属于它自己的独立数组拷贝。
  当数组仅被一个变量引用时，则不会有拷贝发生。
- 在上一个示例的最后，b和c都引用了同一个数组。此时在b上调用unshare方法则会将b变成一个唯一个拷贝：
-*/
+ 在上一个示例的最后，b和c都引用了同一个数组。此时在b上调用unshare方法则会将b变成一个唯一个拷贝.
+ */
 
 var a = [1, 2, 3]
 var b = a
@@ -33,33 +32,31 @@ a[0] = 42
 /*: 
  然而，当你给a附加新元素时，数组的长度会改变。当附加元素这一事件发生时，Swift 会创建这个数组的一个拷贝。从此以后，a将会是原数组的一个独立拷贝。
  拷贝发生后，如果再修改a中元素值的话，a将会返回与b，c不同的结果，因为后两者引用的是原来的数组：
-*/
+ */
 a.append(4)
 a[0] = 777
 (a[0])
 (b[0])
 (c[0])
-
-/*
+/*:
  判定两个数组是否共用相同元素
  我们通过使用恒等运算符(identity operators)( === and !==)来判定两个数组或子数组共用相同的储存空间或元素。
  */
-
-
 import UIKit
+/*: 
+ Arrays and dictionaries in swift use generics and can be mutable or immutable
+ depending on whether they are assigned to a var or let
+ Structs are VALUE types, which means that when working with mutating functions, you'll need to store them in "var". Everytime the struct is "mutated", a new struct will be created and stored in that var.
+ */
 
-// Arrays and dictionaries in swift use generics and can be mutable or immutable
-// depending on whether they are assigned to a var or let
-// Structs are VALUE types, which means that when working with mutating functions, you'll need to store them in "var". Everytime the struct is "mutated", a new struct will be created and stored in that var.
-
-//Arrays
+//: ## Arrays
 var shoppingList: [String] = ["Eggs", "Pigs"]
 shoppingList = ["Eggs", "Pigs"] //Both are the same
 
 if shoppingList.isEmpty { //Checks if count == 0
-    print("The shopping list is empty.")
+    ("The shopping list is empty.")
 } else {
-    print("The shopping list is not empty.")
+    ("The shopping list is not empty.")
 }
 
 shoppingList.append("Cow") //At the end of the array
@@ -96,10 +93,10 @@ var nsObjectArray = ["Eggs",123,true] //推断为NSObject数组
 var food=["f1:apple","f2:orange","f3:banana","v1:tomato","v2:potato"]
 for fruit in food { //fru为Let类型
     if fruit.hasPrefix("f"){
-        print("f_fruit: \(fruit)")
+        ("f_fruit: \(fruit)")
     }
     if fruit.hasSuffix("o"){
-        print("fruit_o: \(fruit)")
+        ("fruit_o: \(fruit)")
     }
 }
 food[3]
@@ -107,6 +104,16 @@ food[3]
 var a10 = Array(0 ... 10) //包含10
 a10 = Array(0 ..< 10) //小于10
 let a10x = (0 ..< 10).map { i in i*i }
+a10x
+let indexOf6 = a10x.indexOf(6)
+let indexOf81 = a10x.indexOf(81)
+let indexOfFirstGreaterThanFive = a10x.indexOf({$0 > 5})
+(indexOfFirstGreaterThanFive)
+let indexOfFirstGreaterThanOneHundred = a10x.indexOf({$0 > 100})
+var filtered = a10.filter { $0 == 3 }  // <= returns [3]
+filtered
+a10.filter { $0 == 3 }.count > 0
+a10.contains(11) == true
 
 //斐波纳契序列
 struct FibonacciSequence : SequenceType {
@@ -133,7 +140,8 @@ nsArray.componentsJoinedByString("-")
 
 
 
-//Dictionaries
+
+//: ## Dictionaries
 
 var airports: [String: String] = ["JFK": "John F. Kennedy", "SCL": "Arturo Merino Benitez"]
 airports = ["JFK": "John F. Kennedy", "SCL": "Arturo Merino Benitez"] //Also valid
@@ -143,9 +151,9 @@ airports.updateValue("Los Angeles", forKey:"LAX") //Updates or creates the value
 airports
 
 if let airportName = airports["LAX"] { //Subscript always returns optional in case value is not set.
-    print("The name of the airport is \(airportName).")
+    ("The name of the airport is \(airportName).")
 } else {
-    print("That airport is not in the airports dictionary.")
+    ("That airport is not in the airports dictionary.")
 }
 
 airports["LAX"] = nil
@@ -155,15 +163,15 @@ airports["HZ"] = "Hang Zhou" //不存在的Key设置Value,dict自动添加k-v对
 //迭代
 //Iterating over the whole dictionary
 for (key, value) in airports {
-    print("\(key): \(value)")
+    ("\(key): \(value)")
 }
 //Iterating on Keys
 for airportCode in airports.keys {
-    print("Airport code: \(airportCode)")
+    ("Airport code: \(airportCode)")
 }
 //Iterating on Values
 for airportName in airports.values {
-    print("Airport name: \(airportName)")
+    ("Airport name: \(airportName)")
 }
 
 //空字典
@@ -182,7 +190,7 @@ person["age"]
 
 var height:String? = person["height"]
 if height != nil {
-    print(height)
+    (height)
 }
 
 var dict = Dictionary<String,Int>()
