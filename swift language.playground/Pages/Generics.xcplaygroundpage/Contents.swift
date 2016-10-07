bde8f6c7487bdad59e57a25a1cbfa603bf9c7994
rockgarden <rockgarden@sina.com>
2016-10-07 20:45:39 +0800

    add

--- swift language.playground/Pages/Generics.xcplaygroundpage/Contents.swift
+++ swift language.playground/Pages/Generics.xcplaygroundpage/Contents.swift
@@ -2,6 +2,146 @@
 
 //: # Generic Functions 泛型函数
 import UIKit
+
+let s: Optional<String> = "howdy"
+
+protocol Flier {
+    func flockTogetherWith(f: Self)
+}
+struct Bird: Flier {
+    func flockTogetherWith(f: Bird) { }
+}
+protocol Flier2 {
+    associatedtype Other
+    func flockTogetherWith(f: Self.Other) // just showing that this is legal
+    func mateWith(f: Other)
+}
+struct Bird2: Flier2 {
+    func flockTogetherWith(f: Bird2) { }
+    func mateWith(f: Bird2) { }
+}
+
+func takeAndReturnSameThing<T> (t: T) -> T {
+    return t
+}
+let thing = takeAndReturnSameThing("howdy")
+
+struct HolderOfTwoSameThings<T> {
+    var firstThing: T
+    var secondThing: T
+    init(thingOne: T, thingTwo: T) {
+        self.firstThing = thingOne
+        self.secondThing = thingTwo
+    }
+}
+let holder = HolderOfTwoSameThings(thingOne: "howdy", thingTwo: "getLost")
+
+func flockTwoTogether<T, U>(f1: T, _ f2: U) { }
+let vd: Void = flockTwoTogether("hey", 1)
+
+protocol Superflier3 { }
+//: 协议在Swift中有两个目的，第一个目的是用来实现多继承(swift语言被设计为单继承的），第二个目的是强制实现者必须准守自己所指定的泛型约束。关键字associatedtype是用来实现第二个目的的。在GeneratorType中由associatedtype指定的Element，是用来控制next()方法的返回类型的。而不是用来指定GeneratorType的类型的。
+protocol Flier3: Superflier3 {
+    associatedtype Other: Superflier3 //声明一个类型的占位符
+    func flockTogetherWith(f: Other)
+}
+struct Bird3: Flier3 {
+    func flockTogetherWith(f: Insect3) { }
+}
+struct Insect3: Flier3 {
+    func flockTogetherWith(f: Insect3) { }
+}
+
+//*/
+
+func flockTwoTogether2<T: Flier3>(f1: T, _ f2: T) { }
+let vd2: Void = flockTwoTogether2(Bird3(), Bird3())
+// let vd3 : Void = flockTwoTogether2(Bird3(), Insect3())
+// let vd4 : Void = flockTwoTogether2("hey", "ho")
+
+func myMin<T: Comparable>(things: T...) -> T {
+    var minimum = things[0]
+    for ix in 1..<things.count {
+        if things[ix] < minimum { // compile error if you remove Comparable constraint
+            minimum = things[ix]
+        }
+    }
+    return minimum
+}
+
+// a generic protocol like Flier3 cannot be used as a type
+// func flockTwoTogether3(f1:Flier3, f2:Flier3) {}
+// it can _only_ be used as a type constraint, as in flockTwoTogether2
+
+protocol Flier4 {
+    associatedtype Other
+}
+struct Bird4: Flier4 {
+    typealias Other = String
+}
+
+class Dog<T> {
+    var name: T?
+}
+let d = Dog<String>()
+
+protocol Flier5 {
+    init()
+}
+struct Bird5: Flier5 {
+    init() { }
+}
+struct FlierMaker<T: Flier5> {
+    static func makeFlier() -> T {
+        return T() // a little surprising I don't have to say T.init(), but I guess it is a real type
+    }
+}
+let f = FlierMaker<Bird5>.makeFlier() // returns a Bird5
+
+class NoisyDog: Dog<String> { } // yes! This is new in Swift 2.0
+class NoisyDog2<T>: Dog<T> { } // and this is also legal!
+// class NoisyDog3 : Dog<T> {} // but this is not; the superclass generic must be resolved somehow
+
+struct Wrapper<T> { }
+struct Wrapper2<T> {
+    var thing: T
+}
+class Cat { }
+class CalicoCat: Cat { }
+
+protocol Flier6 {
+    associatedtype Other
+    func fly()
+}
+
+/*
+ func flockTwoTogether6(f1:Flier6, _ f2:Flier6) { // compile error
+ f1.fly()
+ f2.fly()
+ }
+ */
+
+func flockTwoTogether6<T1: Flier6, T2: Flier6>(f1: T1, _ f2: T2) {
+    f1.fly()
+    f2.fly()
+}
+
+// just testing: this one actually segfaults
+// but not any more! In Swift 2.2 (Xcode 7.3b) this is fine; not sure when that happened
+class Dog2<T: Dog2> { }
+let min = myMin(4, 1, 5, 2)
+(min)
+do {
+    // let w : Wrapper<Cat> = Wrapper<CalicoCat>() // error
+    var w2: Wrapper2<Cat> = Wrapper2(thing: CalicoCat()) // fine
+    let w3 = Wrapper2(thing: CalicoCat())
+    // w2 = w3 // error
+    // ==== shut up the compiler
+    w2 = Wrapper2(thing: CalicoCat())
+    _ = w2
+    _ = w3
+}
+
 //: ## Basic example - 栈(Stack)的操作
 func swapTwoValues<T>(inout a: T, inout _ b: T) { // "_"方法调用时可不申明参数b
     let temporaryA = a
