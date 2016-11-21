455caca541b24e83ad7c9ab4a55f199e1d8226ce
rockgarden <rockgarden@sina.com>
2016-11-21 23:27:10 +0800

    add

--- swift language.playground/Pages/Closures.xcplaygroundpage/Contents.swift
+++ swift language.playground/Pages/Closures.xcplaygroundpage/Contents.swift
@@ -1,62 +1,8 @@
 //: [Previous](@previous)
 import UIKit
-/*:
- # CLOSURES 闭包 = unnamed functions 匿名函数
- 闭包这就是所谓的闭合并包裹着这些常量和变量,俗称闭包.
- 闭包是一个很轻量但是功能十分强大的函数,可以捕获和存储其所在上下文中任意常量和变量的引用,常用于类间的值传递,闭包通常作为函数的参数来使用,当然也可以作为变量.
- Swift会为您管理在捕获过程中涉及到的所有内存操作。
- 闭包采取如下三种形式之一：
- - 全局函数是一个有名字但不会捕获任何值的闭包
- - 嵌套函数是一个有名字并可以捕获其封闭函数域内值的闭包
- - 闭包表达式是一个利用轻量级语法所写的可以捕获其上下文中变量或常量值的匿名闭包
- 闭包表达式:
- { (parameters) -> returnType in statements }
- eg. () -> Void
- */
-//func animateWithDuration(duration: NSTimeInterval, animations: () -> Void)
-var button = UIButton()
-UIView.animateWithDuration(10.0, animations: {
-    button.alpha = 0
-})
-/*:
- ## INLINE CLOSURES
- 内联闭包
- */
-let array = ["John", "Tim", "Steve", "Wangkan"]
-// func sort(@noescape isOrderedBefore: (Self.Generator.Element, Self.Generator.Element) -> Bool) -> [Self.Generator.Element]
-var reversed = array.sort({(s1: String, s2: String) -> Bool in return s1 > s2})
-reversed
-/*:
- ### 闭包特性一: 类型推断
- - 使用类型推断,可以省略参数类型和返回类型;
- - Using type inference, can omit the params and return types.
- - This is true when passing closures as params to a function.
- */
-reversed = array.sort({s1, s2 in return s1 < s2})
-reversed
-//: 如果一个闭包中有一个或多个参数，Swift 允许我们通过参数名来访问参数
-func say1(message: String, completion: (goodbye: String, name: String, age: Int) -> Void) {
-    //print(message)
-    completion(goodbye: "Goodbye", name: "wk", age: 40)
-}
-// 这下面的例子中，我们的尾部闭包中有一个名为goodbye的String型参数，Xcode会将这个参数放到一个元组中，然后紧跟着一个类型代表返回值，最后再加上in关键字来代表参数的结束。
-say1("Hi",completion: {(goodbye: String, name: String, age: Int) -> Void in
-    print(goodbye)
-})
-say1("Hello") { (goodbye, name, age) -> Void in
-    print(goodbye,name,age)
-}
-// (goodbye: String) -> Void in 省略, 省略goodbye的参数名，以及Void返回值,并且in 关键字也可省略
-say1("good") { print($0,$1,$2) }
 
-/*:
- ### 闭包特性二: 单个表达式闭包,返回值是隐式的,因此可以省略返回表达式.
- In case of single-expression closures, the return value is implicit,
- thus the return expression can be omitted.
- 包函数体内如果是单一表达式,还可以省去return语句
- */
-reversed = array.sort({s1, s2 in s1 == s2})
-reversed
+
+
 
 /*:
  ### 闭包特性三: 参数名称缩写
@@ -88,16 +34,6 @@ squreContent1(100)
 squreContent2(100)
 squreContent3(100)
 
-/*:
- ## TRAILING CLOSURES
- 闭包特性四:尾随（Trailing）闭包语法
- 方法中最后一个参数是闭包，顾名思义，称之为尾部闭包。
- 尾部闭包允许我们省略参数名，并且能放置在参数表括号以外，进一步简洁代码。
- Closures which are too long to be defined inline.
- 当闭包太长,不适合定义为内联.
- 如果您需要将一个很长的闭包表达式作为最后一个参数传递给函数,可以使用尾随闭包来增强函数的可读性.
- 尾随闭包是一个书写在函数括号之后的闭包表达式,函数支持将其作为最后一个参数调用.
- */
 func someFunctionThatTakesAClosure(closure: () -> ()) {
     // function body goes here
 }
@@ -136,7 +72,7 @@ let digitNames = [
 ]
 let numbers = [16, 58, 510]
 
-//: - Note: In case the function doesn't take any params other than a trailing closure, there's no need for parenthesis.如果函数除了trailing closure不接受任何参数，就不需要括号。
+//: - Note: In case the function doesn't take any params other than a trailing closure, there's no need for parenthesis. 如果函数除了trailing closure不接受任何参数，就不需要括号。
 let stringsArray = numbers.map {
     (number) -> String in
     var number = number
--- swift language.playground/Pages/Temp.xcplaygroundpage/Contents.swift
+++ swift language.playground/Pages/Temp.xcplaygroundpage/Contents.swift
@@ -2,6 +2,241 @@
 
 import UIKit
 
+/*:
+ # CLOSURES 闭包 
+ = unnamed functions 匿名函数
+ 
+ 闭包
+ - 闭合并包裹着常量和变量, 俗称闭包.
+ - 是一个很轻量但是功能十分强大的函数, 可以捕获和存储其所在上下文中任意常量和变量的引用, 常用于类间的值传递, 闭包通常作为函数的参数来使用, 当然也可以作为变量.
+ - Swift会为您管理在捕获过程中涉及到的所有内存操作。
+ 
+ 闭包采取如下三种形式之一：
+ - 全局函数是一个有名字但不会捕获任何值的闭包
+ - 嵌套函数是一个有名字并可以捕获其封闭函数域内值的闭包
+ - 闭包表达式是一个利用轻量级语法所写的可以捕获其上下文中变量或常量值的匿名闭包
+ 闭包表达式: { (parameters) -> returnType in statements }
+ eg. () -> Void
+ */
+do {
+    func animateWithDuration(duration: TimeInterval, animations: () -> Void) {}
+
+    var button = UIButton()
+    UIView.animate(withDuration: 1, animations: {
+        button.alpha = 0
+    })
+}
+
+/*:
+ ## 闭包特性 - 类型推断
+ - 使用类型推断, 可以省略参数类型和返回类型;
+ - Using type inference, can omit the params and return types.
+ - This is true when passing closures as params to a function.
+ */
+do {
+    let array = ["John", "Tim", "Steve", "Wangkan"]
+    array.sorted(by: {(s1: String, s2: String) -> Bool in return s1 > s2})
+    array.sorted(by: {(s1, s2) -> Bool in return s1 > s2})
+    array
+}
+
+/*:
+ ## 闭包特性 - 单个表达式闭包, 返回值是隐式的, 因此可以省略返回表达式.
+ In case of single-expression closures, the return value is implicit,
+ thus the return expression can be omitted.
+ 包函数体内如果是单一表达式, 还可以省去return语句
+ */
+do {
+    var array = ["John", "Wangkan", "Steve", "Wangkan"]
+    array.sort(by: {(s1, s2) in s1 == s2})
+    array.sort(by: {s1, s2 in s1 == s2})
+    array.sort(by: ==)
+    array
+}
+
+/*: 
+ swift 2: 如果一个闭包中有一个或多个参数，Swift 允许我们通过参数名来访问参数.
+ swift 3: ?! like down
+ 
+ 这下面的例子中，我们的尾部闭包中有一个名为goodbye的String型参数，Xcode会将这个参数放到一个元组中，然后紧跟着一个类型代表返回值，最后再加上in关键字来代表参数的结束。
+*/
+do {
+    func say(_ message: String, completion: (String, String, Int) -> Void) {
+        (message)
+        completion("Goodbye", "wk", 40)
+    }
+//    say("Hi", completion: {("Goodbye", "wk", 40) -> Void in
+//        print($0,$1,$2)
+//    })
+    /// () -> Void in 可省略.
+    say("good") { print($0,$1,$2) }
+}
+
+/*:
+ ## INLINE CLOSURES
+ 内联闭包
+ */
+do {
+    let array = ["John", "Tim", "Steve", "Wangkan"]
+    /// func sorted new [Element]
+    array.sorted()
+    array
+    array.sorted(by: > )
+    array
+    var arr = array
+    /// mutating func sort - so let "array" can't use
+    arr.sort(by: <)
+}
+
+/*:
+ ## TRAILING CLOSURES
+ 方法中最后一个参数是闭包，顾名思义，称之为尾部(Trailing)闭包。
+ 尾部闭包允许我们省略参数名，并且能放置在参数表括号以外，进一步简洁代码。
+
+ 当闭包太长, 不适合定义为内联. Closures which are too long to be defined inline.
+
+ 如果您需要将一个很长的闭包表达式作为最后一个参数传递给函数, 可以使用尾随闭包来增强函数的可读性.
+
+ 尾随闭包是一个书写在函数括号之后的闭包表达式, 函数支持将其作为最后一个参数调用.
+ */
+do { }
+
+//: - Note: In case the function doesn't take any params other than a trailing closure, there's no need for parenthesis.如果函数除了trailing closure不接受任何参数，就不需要括号。
+do {
+    let array = ["John", "Tim", "Steve", "Wangkan"]
+    array.sorted{(s1, s2) -> Bool in return s1 > s2}
+    array
+    var arr = array
+    arr.sort{(s1, s2) -> Bool in return s1 > s2}
+    arr
+}
+
+do {
+    enum HTTPResponse {
+        case ok
+        case error(Int)
+    }
+
+    var responses: [HTTPResponse] = [.error(500), .ok, .ok, .error(404), .error(403)]
+
+    let sortedResponses = responses.sorted {
+        switch ($0, $1) {
+        // Order errors by code
+        case let (.error(aCode), .error(bCode)):
+            return aCode < bCode
+
+        // All successes are equivalent, so none is before any other
+        case (.ok, .ok): return false
+
+        // Order errors before successes
+        case (.error, .ok): return true
+        case (.ok, .error): return false
+        }
+    }
+    (sortedResponses)
+    (responses)
+
+    responses.sort {
+        switch ($0, $1) {
+        // Order errors by code
+        case let (.error(aCode), .error(bCode)):
+            return aCode < bCode
+
+        // All successes are equivalent, so none is before any other
+        case (.ok, .ok): return false
+
+        // Order errors before successes
+        case (.error, .ok): return true
+        case (.ok, .error): return false
+        }
+    }
+    (responses)
+}
+
+//: ## closure capture
+//: the rule for @escaping is about a function passed in and then passed out so.
+do {
+    /// this is legal
+    func funcCaller(f: ()->()) {
+        f()
+    }
+
+    func funcMaker() -> () -> () {
+        return { print("hello world") }
+    }
+
+    /// but this is not legal, without @escaping
+    func funcPasser(f: @escaping ()->()) -> () -> () {
+        return f
+    }
+
+    class ViewController: UIViewController {
+        func test() {
+            func f() {
+                print(presentingViewController!)
+            }
+            funcCaller(f: f) // okay
+            let f2 = funcPasser(f: f) // okay, even though f doesn't say "self"
+            // I regard that as a bug (and I think so does Jordan Rose)
+            let f3 = funcPasser {
+                print(self.presentingViewController!) // self required
+            }
+
+            let _ = (f2,f3)
+        }
+
+        func pass100 (_ f:(Int)->()) {
+            f(100)
+        }
+
+        func countAdder(_ f: @escaping ()->()) -> () -> () {
+            var ct = 0
+            return {
+                ct = ct + 1
+                print("count is \(ct)")
+                f()
+            }
+        }
+
+        override func viewDidLoad() {
+            super.viewDidLoad()
+            var x = 0
+            print(x)
+            func setX(newX:Int) {
+                x = newX
+            }
+            pass100(setX)
+            print(x)
+
+            func greet () {
+                print("howdy")
+            }
+            let countedGreet = countAdder(greet)
+            countedGreet() // 1
+            countedGreet() // 2
+            countedGreet() // 3
+
+            do {
+                let countedGreet = countAdder(greet)
+                let countedGreet2 = countAdder(greet)
+                countedGreet() // count is 1
+                countedGreet2() // count is 1
+            }
+
+            do { // showing that functions are reference types
+                let countedGreet = countAdder(greet)
+                let countedGreet2 = countedGreet
+                countedGreet() // count is 1
+                countedGreet2() // count is 2
+            }
+        }
+ 
+    }
+
+}
+
+
+
 class Dog {
     var whatThisDogSays = "woof"
     func bark() {
@@ -22,8 +257,6 @@ func doThis(_ f: ()->()) { // f : Void -> Void
 doThis { // no parentheses!
     print("Howdy")
 }
-
-
 
 func imageOfSize(_ size:CGSize, _ whatToDraw: () -> ()) -> UIImage {
     UIGraphicsBeginImageContextWithOptions(size, false, 0)
