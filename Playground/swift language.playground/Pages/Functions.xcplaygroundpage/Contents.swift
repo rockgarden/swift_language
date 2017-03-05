394ec57cd2ba976e52cb9402138a57866f6ed76f
rockgarden <rockgarden@sina.com>
2017-03-05 16:34:18 +0800

    add

--- Playground/swift language.playground/Pages/Functions.xcplaygroundpage/Contents.swift
+++ Playground/swift language.playground/Pages/Functions.xcplaygroundpage/Contents.swift
@@ -4,15 +4,22 @@ import UIKit
 
 //: # Functions
 /*: 
- ## 语法
+ ## Defining and Calling Functions
  these DO just illustrates some declaration syntax
+ - All of this information is rolled up into the function’s definition, which is prefixed with the func keyword. 
+ - You indicate the function’s return type with the return arrow -> (a hyphen followed by a right angle bracket), which is followed by the name of the type to return.
  */
-do {
-    func sayHello(personName: String) -> String{
-        return "Hello \(personName)!"
-    }
-    sayHello(personName: "Juan")
+func greet(person: String) -> String {
+    let greeting = "Hello, " + person + "!"
+    return greeting
 }
+print(greet(person: "Anna"))
+print(greet(person: "Brian"))
+
+func greetAgain(person: String) -> String {
+    return "Hello again, " + person + "!"
+}
+print(greetAgain(person: "Anna"))
 
 do {
     func sum(_ x:Int, _ y:Int) -> Int {
@@ -75,40 +82,197 @@ do {
     greet()
 }
 
-/*:
- ## parameter Void
- - 无参Function相当于Void参数?
- - passing a void is the same as no parameters
- - Void 可省略
- - Void 多用于 Closures
- return Void
- - translates in returning an empty tuple: ()
- - 没有定义返回类型的函数会返回特殊的值Void,它其实是一个空的元组tuple,没有任何元素,也可简写成()
- */
+
+//: ## Function Parameters and Return Values
+
+//: ### Functions Without Parameters
 do {
-    func say1(_ s:String) -> Void { (s) }
-    func say2(_ s:String) -> () { (s) }
-    func say3(_ s:String) { (s) }
-    say1("howdy")
-    say2("howdy")
-    say3("howdy")
-    let pointless : Void = say1("howdy") //showing that we actually return void
-    ("pointless is \(pointless)") //showing that we captured the returned void
-}
-do {
-    func greet1(_ unused: Void) -> Void { ("howdy") }
-    func greet2() -> () { ("howdy") }
-    func greet3() { ("howdy") }
-    func greet4()-> String { return ("howdy") }
-    greet1()
-    greet2()
-    greet3()
-    let v:Void = ()
-    greet1(v)
-    greet4(v)
+    func sayHelloWorld() -> String {
+        return "hello, world"
+    }
+    print(sayHelloWorld())
 }
 
-//: ## Parameters
+//: ### Functions With Multiple Parameters
+func greet(person: String, alreadyGreeted: Bool) -> String {
+    if alreadyGreeted {
+        return greetAgain(person: person)
+    } else {
+        return greet(person: person)
+    }
+}
+print(greet(person: "Tim", alreadyGreeted: true))
+
+//: ### Functions Without Return Values
+/*:
+ - NOTE:
+ Strictly speaking, this version of the greet(person:) function does still return a value, even though no return value is defined. Functions without a defined return type return a special value of type Void. This is simply an empty tuple, which is written as ().
+ */
+do {
+    /// 相当于 -> ()
+    func greet(person: String) {
+        print("Hello, \(person)!")
+    }
+    print(greet(person: "Dave"))
+}
+/*:
+ - NOTE:
+ Return values can be ignored, but a function that says it will return a value must always do so. A function with a defined return type cannot allow control to fall out of the bottom of the function without returning a value, and attempting to do so will result in a compile-time error.
+ */
+do {
+    func printAndCount(string: String) -> Int {
+        print(string)
+        return string.characters.count
+    }
+    func printWithoutCounting(string: String) {
+        let _ = printAndCount(string: string)
+    }
+    printAndCount(string: "hello, world")
+    printWithoutCounting(string: "hello, world")
+    // prints "hello, world" but does not return a value
+}
+
+//: ### Functions with Multiple Return Values
+do {
+    /// returns a tuple containing two Int values.
+    func minMax(array: [Int]) -> (min: Int, max: Int) {
+        var currentMin = array[0]
+        var currentMax = array[0]
+        for value in array[1..<array.count] {
+            if value < currentMin {
+                currentMin = value
+            } else if value > currentMax {
+                currentMax = value
+            }
+        }
+        return (currentMin, currentMax)
+    }
+    let bounds = minMax(array: [8, -6, 2, 109, 3, 71])
+    print("min is \(bounds.min) and max is \(bounds.max)")
+}
+
+//: ### Optional Tuple Return Types
+/*:
+ - NOTE:
+ An optional tuple type such as (Int, Int)? is different from a tuple that contains optional types such as (Int?, Int?). With an optional tuple type, the entire tuple is optional, not just each individual value within the tuple.
+ */
+do {
+    func minMax(array: [Int]) -> (min: Int, max: Int)? {
+        if array.isEmpty { return nil }
+        var currentMin = array[0]
+        var currentMax = array[0]
+        for value in array[1..<array.count] {
+            if value < currentMin {
+                currentMin = value
+            } else if value > currentMax {
+                currentMax = value
+            }
+        }
+        return (currentMin, currentMax)
+    }
+    if let bounds = minMax(array: [8, -6, 2, 109, 3, 71]) {
+        print("min is \(bounds.min) and max is \(bounds.max)")
+    }
+}
+
+
+//: ## Function Argument Labels and Parameter Names
+func someFunction(firstParameterName: Int, secondParameterName: Int) {
+    // In the function body, firstParameterName and secondParameterName
+    // refer to the argument values for the first and second parameters.
+}
+someFunction(firstParameterName: 1, secondParameterName: 2)
+
+//: ### Specifying Argument Labels
+/*:
+ 外部参数名称 external and local params names.
+ - 外部参数名,是为了调用该函数时让其参数更为有表现力,更为通顺，同时还保持了函数体是可读的和有明确意图的,让别人阅读整段代码时更方便
+ */
+do {
+    /// write an argument label before the parameter name, separated by a space:
+    func someFunction(argumentLabel parameterName: Int) {
+        // In the function body, parameterName refers to the argument value
+        // for that parameter.
+    }
+    func greet(person: String, from hometown: String) -> String {
+        return "Hello \(person)!  Glad you could visit from \(hometown)."
+    }
+    print(greet(person: "Bill", from: "Cupertino"))
+}
+
+//: ### Omitting Argument Labels
+do {
+    func someFunction(_ firstParameterName: Int, secondParameterName: Int) {
+        // In the function body, firstParameterName and secondParameterName
+        // refer to the argument values for the first and second parameters.
+    }
+    someFunction(1, secondParameterName: 2)
+}
+
+//: ### Default Parameter Values
+/*:
+ - 预定义参数, 这意味着我们可以在调用该函数时省略它.
+ */
+do {
+    func someFunction(parameterWithoutDefault: Int, parameterWithDefault: Int = 12) {
+        // If you omit the second argument when calling this function, then
+        // the value of parameterWithDefault is 12 inside the function body.
+    }
+    someFunction(parameterWithoutDefault: 3, parameterWithDefault: 6) // parameterWithDefault is 6
+    someFunction(parameterWithoutDefault: 4) // parameterWithDefault is 12
+
+    func join(string s1: String, toString s2: String, withJoiner joiner: String = "only")
+        -> String {
+            return s1 + joiner + s2
+    }
+    join(string: "Hello", toString: "World", withJoiner: "New")
+    join(string: "", toString: "")
+}
+
+//: ### Variadic Parameters
+/*:
+ - NOTE:
+ A function may have at most one variadic parameter.
+ */
+do {
+    func arithmeticMean(_ numbers: Double...) -> Double {
+        var total: Double = 0
+        for number in numbers {
+            total += number
+        }
+        return total / Double(numbers.count)
+    }
+    arithmeticMean(1, 2, 3, 4, 5)
+    arithmeticMean(3, 8.25, 18.75)
+}
+
+
+//: ### In-Out Parameters
+/*:
+ - NOTE:
+ In-out parameters cannot have default values, and variadic parameters cannot be marked as inout.
+ */
+do {
+    func swapTwoInts(_ a: inout Int, _ b: inout Int) {
+        let temporaryA = a
+        a = b
+        b = temporaryA
+    }
+    var someInt = 3
+    var anotherInt = 107
+    swapTwoInts(&someInt, &anotherInt)
+    print("someInt is now \(someInt), and anotherInt is now \(anotherInt)")
+}
+/*:
+ - NOTE:
+ In-out parameters are not the same as returning a value from a function. The swapTwoInts example above does not define a return type or return a value, but it still modifies the values of someInt and anotherInt. In-out parameters are an alternative way for a function to have an effect outside of the scope of its function body.
+ 输入 - 输出参数与从函数返回值不同。 上面的swapTwoInts示例没有定义返回类型或返回值，但它仍然修改someInt和anotherInt的值。 输入 - 输出参数是函数具有超出函数体范围仍有效的替代方法。
+ */
+
+
+
+
+
 do {
     class Dog {
         //: Default Parameter
@@ -151,28 +315,6 @@ do {
     say2("hi", times:3, true)
 }
 
-/*:
- ### 外部和本地参数名称
- Here we are using external and local params names.
- - 外部参数名,是为了调用该函数时让其参数更为有表现力,更为通顺，同时还保持了函数体是可读的和有明确意图的,让别人阅读整段代码时更方便
- - 注意外部名称将自动提供给每一个预定义的参数.
- - 下面join最后一个参数是预先定义的,这意味着我们可以在调用该函数时省略它.
- */
-do {
-    func join(string s1: String, toString s2: String, withJoiner joiner: String = "only")
-        -> String {
-            return s1 + joiner + s2
-    }
-    join(string: "Hello", toString: "World", withJoiner: "New")
-    join(string: "", toString: "")
-
-    var red:CGFloat = 0.0,green:CGFloat=0.0,blue:CGFloat=0.0,alpha:CGFloat=0.0
-    UIColor.red.getRed(&red, green: &green, blue: &blue, alpha: &alpha)
-    red
-    green
-    blue
-    alpha
-}
 
 /*:
  ### var modifiable Parameters
@@ -511,6 +653,41 @@ do {
     }
 }
 
+/*:
+ ## parameter Void
+ - 无参Function相当于Void参数?
+ - passing a void is the same as no parameters
+ - Void 可省略
+ - Void 多用于 Closures
+ return Void
+ - translates in returning an empty tuple: ()
+ - 没有定义返回类型的函数会返回特殊的值Void,它其实是一个空的元组tuple,没有任何元素,也可简写成()
+ */
+do {
+    func say1(_ s:String) -> Void { (s) }
+    func say2(_ s:String) -> () { (s) }
+    func say3(_ s:String) { (s) }
+    say1("howdy")
+    say2("howdy")
+    say3("howdy")
+    let pointless : Void = say1("howdy") //showing that we actually return void
+    ("pointless is \(pointless)") //showing that we captured the returned void
+}
+do {
+    func greet1(_ unused: Void) -> Void { ("howdy") }
+    func greet2() -> () { ("howdy") }
+    func greet3() { ("howdy") }
+    func greet4()-> String { return ("howdy") }
+    greet1()
+    greet2()
+    greet3()
+    let v:Void = ()
+    greet1(v)
+    greet4(v)
+}
+
+
+
 //: ## 函数变量 function As Value
 do {
     func addInts(a:Int,b:Int)->Int {return a+b}
@@ -720,3 +897,11 @@ extension Thing {
 //: [Next](@next)
 
 
+do {
+    var red:CGFloat = 0.0,green:CGFloat=0.0,blue:CGFloat=0.0,alpha:CGFloat=0.0
+    UIColor.red.getRed(&red, green: &green, blue: &blue, alpha: &alpha)
+    red
+    green
+    blue
+    alpha
+}
