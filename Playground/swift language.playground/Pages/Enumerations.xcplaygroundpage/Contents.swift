75b562809f0109db40f3df6f8444ed27fdbe0030
rockgarden <rockgarden@sina.com>
2017-03-06 21:52:56 +0800

    opt

--- Playground/swift language.playground/Pages/Enumerations.xcplaygroundpage/Contents.swift
+++ Playground/swift language.playground/Pages/Enumerations.xcplaygroundpage/Contents.swift
@@ -2,66 +2,162 @@
 import UIKit
 //: # Enum
 /*:
- Definition of an Enum
- In this case, there's no default value for the enum members.
+ An enumeration defines a common type for a group of related values and enables you to work with those values in a type-safe way within your code.
  */
-enum CompassPoint {
-    case North
-    case South
-    case East
-    case West
-}
-//: 等效于
-enum CompassPointA {
-    case North, South, East, West
+//: # Enumeration Syntax
+enum SomeEnumeration {
+    // enumeration definition goes here
 }
 
-var someEnum = CompassPoint.East
-var someEnumA = CompassPointA.East
-/*: 
- 一旦类型是已知的，可以省略多次赋值
- Once the type is known, it can be omitted for reassignment.
- */
-someEnum = .West
-("This is the enum: \(someEnum)")
-(someEnum == .West) ? ("Equal") : ("Not Equal")
-switch(someEnum){
-case .North:
-    CompassPoint.North
-case .South:
-    CompassPoint.South
-case .East:
-    CompassPoint.East
-case .West:
-    CompassPoint.West
+enum CompassPoint {
+    case north
+    case south
+    case east
+    case west
 }
+
+/*
+ - NOTE:
+ Unlike C and Objective-C, Swift enumeration cases are not assigned a default integer value when they are created. In the CompassPoint example above, north, south, east and west do not implicitly equal 0, 1, 2 and 3. Instead, the different enumeration cases are fully-fledged values in their own right, with an explicitly-defined type of CompassPoint.
+ */
+//: Multiple cases can appear on a single line, separated by commas:
+enum Planet {
+    case mercury, venus, earth, mars, jupiter, saturn, uranus, neptune
+}
+do {
+    var directionToHead = CompassPoint.west
+    directionToHead = .east
+}
+
+
+//: # Matching Enumeration Values with a Switch Statement
+do {
+    var directionToHead = CompassPoint.west
+    directionToHead = .south
+    switch directionToHead {
+    case .north:
+        print("Lots of planets have a north")
+    case .south:
+        print("Watch out for penguins")
+    case .east:
+        print("Where the sun rises")
+    case .west:
+        print("Where the skies are blue")
+    }
+}
+do {
+    /// When it is not appropriate to provide a case for every enumeration case, you can provide a default case to cover any cases that are not addressed explicitly
+    let somePlanet = Planet.earth
+    switch somePlanet {
+    case .earth:
+        print("Mostly harmless")
+    default:
+        print("Not a safe place for humans")
+    }
+}
+
+//: # Associated Values 关联值
 /*:
  显式定义枚举成员
  - Defines an enum with members with explicit types.
  */
 enum Barcode {
-    case UPCA(Int, Int, Int, Int)
-    case QRCode(String)
+    case upc(Int, Int, Int, Int)
+    case qrCode(String)
 }
-var newEnum = Barcode.QRCode("162534")
-newEnum = Barcode.QRCode("692438")
+//: mean “Define an enumeration type called Barcode, which can take either a value of upc with an associated value of type (Int, Int, Int, Int), or a value of qrCode with an associated value of type String.”
+do {
+    /// New barcodes can then be created using either type
+    var productBarcode = Barcode.upc(8, 85909, 51226, 3)
+    /// assigned a different type of barcode
+    productBarcode = .qrCode("ABCDEFGHIJKLMNOP")
+
+    switch productBarcode {
+    case .upc(let numberSystem, let manufacturer, let product, let check):
+        print("UPC: \(numberSystem), \(manufacturer), \(product), \(check).")
+    case .qrCode(let productCode):
+        print("QR code: \(productCode).")
+    }
+}
+
+
+//: # Raw Values 原始值
+/*:
+ 在这种情况下,枚举与原始值的定义必须是独一无二的
+ In this case, the enum is defined with raw values.
+ They must be unique.
+ */
+enum ASCIIControlCharacter: Character {
+    case tab = "\t"
+    case lineFeed = "\n"
+    case carriageReturn = "\r"
+}
+do {
+    let asciiCode = ASCIIControlCharacter.lineFeed
+    ("Code is \(asciiCode.rawValue)")
+    /// In this case, since it's not guaranteed to find an enum for the specified rawValue, the initializer returns an optional
+    if let lineFeed = ASCIIControlCharacter(rawValue: "\r") {
+        //Do something
+        lineFeed
+    }
+}
+/*
+ - NOTE:
+ Raw values are not the same as associated values. Raw values are set to prepopulated values when you first define the enumeration in your code, like the three ASCII codes above. The raw value for a particular enumeration case is always the same. Associated values are set when you create a new constant or variable based on one of the enumeration’s cases, and can be different each time you do so.
+ 原始值与关联值不同。 当您首次在代码中定义枚举时，原始值将设置为预填充值，如上面的三个ASCII代码。 特定枚举大小的原始值总是相同的。 当您基于枚举的一种情况创建新的常量或变量时，相关值将被设置，并且每次这样做都可能不同。
+ */
+
+//: ## Implicitly Assigned Raw Values 隐式赋值原始值
+do {
+    /// with integer raw values to represent each planet’s order from the sun:
+    enum Planet: Int {
+        case mercury = 1, venus, earth, mars, jupiter, saturn, uranus, neptune
+    }
+    /// with string raw values to represent each direction’s name:
+    enum CompassPoint: String {
+        case north, south, east, west
+    }
+
+    let earthsOrder = Planet.earth.rawValue
+    let sunsetDirection = CompassPoint.west.rawValue
+}
+
+//: ## Initializing from a Raw Value
+
+
+
+
+
+
+
+
+
+///*: 
+// 一旦类型是已知的，可以省略多次赋值
+// Once the type is known, it can be omitted for reassignment.
+// */
+//someEnum = .west
+//("This is the enum: \(someEnum)")
+//(someEnum == .West) ? ("Equal") : ("Not Equal")
+//switch(someEnum){
+//case .North:
+//    CompassPoint.North
+//case .South:
+//    CompassPoint.South
+//case .East:
+//    CompassPoint.East
+//case .West:
+//    CompassPoint.West
+//}
+
+
 /*:
  在这种情况下,枚举与原始值的定义必须是独一无二的
  In this case, the enum is defined with raw values.
  They must be unique
  */
-enum ASCIIControlCharacter: Character {
-    case Tab = "\t"
-    case LineFeed = "\n"
-    case CarriageReturn = "\r"
-}
-let asciiCode = ASCIIControlCharacter.Tab
-("Code is \(asciiCode.rawValue)")
-//: In this case, since it's not guaranteed to find an enum for the specified rawValue, the initializer returns an optional
-if let lineFeed = ASCIIControlCharacter(rawValue: "\r") {
-    //Do something
-    lineFeed
-}
+
+
 do {
     enum Filter : String {
         case Albums = "Albums"
@@ -84,7 +180,7 @@ do {
             set {}
         }
         mutating func advance() {
-            var ix = Filter.cases.indexOf(self)!
+            var ix = Filter.cases.index(of:self)!
             ix = (ix + 1) % 4
             self = Filter.cases[ix]
         }
@@ -95,11 +191,11 @@ do {
     let type3 = Filter(2) // .Podcasts
     let type4 = Filter(5) // nil
     let type5 = Filter("Playlists")
-    (type5.description)
+    type5?.description
     // type5.s = "test" // compile error
     var type6 = type5
-    type6.s = "test" //no set, so still type5
-    (type6.s)
+    type6?.s = "test" //no set, so still type5
+    (type6?.s)
     var type7 = Filter.Books
     type7.advance() // Filter.Albums, 返回0 = Albums
     (type7)
@@ -164,29 +260,30 @@ do {
 
 var s : String? = "howdy"
 switch s {
-case .Some(let theString):
+case .some(let theString):
     (theString) // howdy
-case .None:
+case .none:
     ("it's nil")
 }
 //: # Example
-enum ShapeMaker {
-    case Rectangle
-    case Ellipse
-    case Diamond
-    func drawShape (p: CGMutablePath, inRect r : CGRect) -> () {
-        switch self {
-        case Rectangle:
-            CGPathAddRect(p, nil, r)
-        case Ellipse:
-            CGPathAddEllipseInRect(p, nil, r)
-        case Diamond:
-            CGPathMoveToPoint(p, nil, r.minX, r.midY)
-            CGPathAddLineToPoint(p, nil, r.midX, r.minY)
-            CGPathAddLineToPoint(p, nil, r.maxX, r.midY)
-            CGPathAddLineToPoint(p, nil, r.midX, r.maxY)
-            CGPathCloseSubpath(p)
-        }
-    }
-}
+//enum ShapeMaker {
+//    case Rectangle
+//    case Ellipse
+//    case Diamond
+//
+//    func drawShape (p: CGMutablePath, inRect r : CGRect) -> () {
+//        switch self {
+//        case .Rectangle:
+//            CGPathAddRect(p, nil, r)
+//        case Ellipse:
+//            CGPathAddEllipseInRect(p, nil, r)
+//        case Diamond:
+//            CGPathMoveToPoint(p, nil, r.minX, r.midY)
+//            CGPathAddLineToPoint(p, nil, r.midX, r.minY)
+//            CGPathAddLineToPoint(p, nil, r.maxX, r.midY)
+//            CGPathAddLineToPoint(p, nil, r.midX, r.maxY)
+//            CGPathCloseSubpath(p)
+//        }
+//    }
+//}
 //: [Next](@next)
